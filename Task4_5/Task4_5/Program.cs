f1eb24d3b948dbc71a8d6b9fa6d03833de29e82d
Maryia Neiman <Maryia_Neiman@epam.com>
2016-07-04 15:00:29 +0300

    second

---
+++ Task4_5/Task4_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine(rectangle.calc(32, 65, 3, 9));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task4_5/Task4_5/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_5
+{
+    class Rectangle
+    {
+
+        public int calc (int W, int H, int w, int h)
+        {
+            return Math.Max(pack(W, H, w, h), pack(W, H, h, w));
+        }
+
+        public int pack (int W, int H, int w, int h)
+        {
+            var n = (W / w | 0) * (H / h | 0);
+            if (W % w >= h && H >= w)
+            {
+                n += pack(W % w, H, h, w);
+            }
+            else if (H % h >= w && W >= h)
+            {
+                n += pack(W, H % h, h, w);
+            }
+            return n;
+        }
+    }
+}
